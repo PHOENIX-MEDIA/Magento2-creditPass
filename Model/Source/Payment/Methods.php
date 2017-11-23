3a2aa1d253b408dc997e50d0811866f17d9d9125
PhoenixDev - JJ <jan.jaworski@at.phoenix-media.eu>
2017-11-23 10:37:48 +0100

    Add missing namespace

--- Model/Source/Payment/Methods.php
+++ Model/Source/Payment/Methods.php
@@ -8,6 +8,8 @@
  * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
+namespace Phoenix\Creditpass\Model\Source\Payment;
+
 class Methods extends \Magento\Payment\Model\Config\Source\Allmethods
 {
     /**
